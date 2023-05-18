1cb9544a82b2fba19c08a56e77a98c3de92047f9
Sonny To <son.c.to@gmail.com>
2023-05-17 19:49:39 -0500

    start/stop server

--- src/clj/lambdacart/core.clj
+++ src/clj/lambdacart/core.clj
@@ -1,5 +1,11 @@
 (ns lambdacart.core
-  (:require [stigmergy.chp]))
+  (:require [stigmergy.plumdb :as db]
+            [stigmergy.chp]
+            [stigmergy.server :as s]))
 
 (defn default-handler [req]
   (stigmergy.chp/render "mdb-template.chp" {}))
+
+(comment
+  (s/start-http)
+  (s/stop-http))
