c06bb08d52663d407f0919cb2e17bd4fee7c76be
Sonny To <son.c.to@gmail.com>
2025-05-18 13:27:34 +0700

    chp-dir public-dir

--- config.clj
+++ config.clj
@@ -1,15 +1,4 @@
-(load-file "src/clj/lambdacart/core.clj")
-
-{:environment :production
- :port 3001
- :chp-dir "public"
+{:port 3000
+ :chp-dir "public/chp"
  :public-dir "public"
- :index "mdb-template.chp"
- :mime-types {"chp" "text/html" 
-              "blog" "text/html"
-              nil "text/html"}
- :bidi-routes ["/" [
-                    ["" (fn [req]
-                          (lambdacart.core/default-handler {}))]
-                    #_[#".*\.chp"  stigmergy.chp/hiccup-page-handler]]]}
-
+ :bidi-routes ["/" [[#".*\.chp"  stigmergy.chp/hiccup-page-handler]]]}
