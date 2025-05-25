5196bb474cef2329a78efc9c8cb3d469d82b76da
Sonny To <son.c.to@gmail.com>
2025-05-25 21:09:08 +0700

    arrow key navigation of grid

--- src/cljs/lambdacart/grid.cljs
+++ src/cljs/lambdacart/grid.cljs
@@ -1,5 +1,39 @@
 (ns lambdacart.grid)
 
-(defn init []
-  #_(js/alert "hello-connor2")
-  (prn "cljs init"))
+(defn find-target-cell [current-cell direction]
+  (let [row (.closest current-cell "div[style*='display: flex']")
+        cells (array-seq (.querySelectorAll row "[contenteditable='true']"))
+        rows (array-seq (.querySelectorAll js/document "div[style*='display: flex']"))
+        current-index (.indexOf (vec cells) current-cell)
+        current-row-index (.indexOf (vec rows) row)]
+    (case direction
+      :left  (when (pos? current-index)
+               (nth cells (dec current-index)))
+      :right (when (< current-index (dec (count cells)))
+               (nth cells (inc current-index)))
+      :up    (when (pos? current-row-index)
+               (let [target-row (nth rows (dec current-row-index))
+                     target-cells (array-seq (.querySelectorAll target-row "[contenteditable='true']"))] ; Added array-seq here
+                 (nth target-cells current-index)))
+      :down  (when (< current-row-index (dec (count rows)))
+               (let [target-row (nth rows (inc current-row-index))
+                     target-cells (array-seq (.querySelectorAll target-row "[contenteditable='true']"))] ; Added array-seq here
+                 (nth target-cells current-index))))))
+
+(defn handle-key-nav [e]
+  (let [key->direction {"ArrowLeft" :left
+                        "ArrowRight" :right
+                        "ArrowUp" :up
+                        "ArrowDown" :down}
+        direction (get key->direction (.-key e))]
+    (when direction
+      (.preventDefault e)
+      (when-let [target (find-target-cell (.-target e) direction)]
+        (.focus target)))))
+
+;; Initialize event listener
+(defn init! []
+  (.addEventListener js/document "keydown"
+                     (fn [e]
+                       (when (.-contentEditable (.-target e))
+                         (handle-key-nav e)))))
