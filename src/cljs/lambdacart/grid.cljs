ee7938c0931ea19ac0efeec3bb63245bf29feca2
Sonny To <son.c.to@gmail.com>
2025-05-25 22:27:43 +0700

    switch to clientside rendering with reagent

--- src/cljs/lambdacart/grid.cljs
+++ src/cljs/lambdacart/grid.cljs
@@ -1,4 +1,20 @@
-(ns lambdacart.grid)
+(ns lambdacart.grid
+  (:require [reagent.core :as r]
+            [reagent.dom.client :as rdc]))
+
+(defn header []
+  (let [style {:style {:flex "1" :padding "10px" :border-right "1px solid #ddd"}}]
+    [:div {:style {:display "flex"
+                   :position "sticky"
+                   :top "0"
+                   :background "#f0f0f0"
+                   :z-index "1"
+                   :font-weight "bold"
+                   :border-bottom "1px solid #ccc"}}
+     [:div {:style {:width "20px" :height "20px" :padding "10px" :border-right "1px solid #ddd"}} ""]
+     [:div style "Tour Name"]
+     [:div style "Description"]
+     [:div style "Image"]]))
 
 (defn find-target-cell [current-cell direction]
   (let [row (.closest current-cell "div[style*='display: flex']")
@@ -31,8 +47,40 @@
       (when-let [target (find-target-cell (.-target e) direction)]
         (.focus target)))))
 
+(defn grid-component []
+  (let [rows (r/atom (vec (for [i (range 100)]
+                           [(rand-int 100) i i])))
+        sorted-rows (sort-by first @rows)]
+    (fn []
+      [:div
+       [header]
+       [:div {:style {:width "100%"
+                      :height "90%"
+                      :border "1px solid #ccc"
+                      :overflow "auto"
+                      :font-family "sans-serif"
+                      :font-size "14px"
+                      :position "relative"}}
+        (for [i (range 100)
+              :let [style {:flex "1" :padding "8px" :border-bottom "1px solid #eee" :border-right "1px solid #f9f9f9"}
+                    prop {:contenteditable "true"
+                          :style style}]]
+          (into [:div {:style {:display "flex"}
+                      :key i}
+                [:div {:style {:width "20px" :height "20px" :padding "10px" :border-right "1px solid #ddd"}} (inc i)]]
+                (let [row (nth sorted-rows i)]
+                  [[:div prop (str (nth row 0))]
+                   [:div prop (str (nth row 1))]
+                   [:div (assoc prop :style (assoc (:style prop) :border-right "none"))
+                    (str (nth row 2))]])))]])))
+
+(defonce root (atom nil))
+
+(defn mount-grid []
+  (when-let [container (.getElementById js/document "app")]
+    (when-not @root
+      (reset! root (rdc/create-root container)))
+    (rdc/render @root [grid-component])))
+
 (defn init! []
-  (.addEventListener js/document "keydown"
-                     (fn [e]
-                       (when (.-contentEditable (.-target e))
-                         (handle-key-nav e)))))
+  (mount-grid))
