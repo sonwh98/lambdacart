c39ee025d02decc94471139419633e09bd2c1dab
Sonny To <son.c.to@gmail.com>
2025-05-26 11:29:40 +0700

    change grid-component a function of rows

--- src/cljs/lambdacart/grid.cljs
+++ src/cljs/lambdacart/grid.cljs
@@ -18,7 +18,7 @@
 
 (defn find-target-cell [current-cell direction]
   (let [row (.closest current-cell "div[style*='display: flex']")
-        cells (array-seq (.querySelectorAll row "[contenteditable='true']"))
+        cells (array-seq (.querySelectorAll row "[contentEditable='true']"))
         rows (array-seq (.querySelectorAll js/document "div[style*='display: flex']"))
         current-index (.indexOf (vec cells) current-cell)
         current-row-index (.indexOf (vec rows) row)]
@@ -29,11 +29,11 @@
                (nth cells (inc current-index)))
       :up    (when (pos? current-row-index)
                (let [target-row (nth rows (dec current-row-index))
-                     target-cells (array-seq (.querySelectorAll target-row "[contenteditable='true']"))]
+                     target-cells (array-seq (.querySelectorAll target-row "[contentEditable='true']"))]
                  (nth target-cells current-index)))
       :down  (when (< current-row-index (dec (count rows)))
                (let [target-row (nth rows (inc current-row-index))
-                     target-cells (array-seq (.querySelectorAll target-row "[contenteditable='true']"))] 
+                     target-cells (array-seq (.querySelectorAll target-row "[contentEditable='true']"))] 
                  (nth target-cells current-index))))))
 
 (defn handle-key-nav [e]
@@ -47,32 +47,30 @@
       (when-let [target (find-target-cell (.-target e) direction)]
         (.focus target)))))
 
-(defn grid-component []
-  (let [rows (r/atom (vec (for [i (range 100)]
-                           [(rand-int 100) i i])))
-        sorted-rows (sort-by first @rows)]
-    (fn []
-      [:div
-       [header]
-       [:div {:style {:width "100%"
-                      :height "90%"
-                      :border "1px solid #ccc"
-                      :overflow "auto"
-                      :font-family "sans-serif"
-                      :font-size "14px"
-                      :position "relative"}}
-        (for [i (range 100)
-              :let [style {:flex "1" :padding "8px" :border-bottom "1px solid #eee" :border-right "1px solid #f9f9f9"}
-                    prop {:contenteditable "true"
-                          :style style}]]
-          (into [:div {:style {:display "flex"}
-                      :key i}
-                [:div {:style {:width "20px" :height "20px" :padding "10px" :border-right "1px solid #ddd"}} (inc i)]]
-                (let [row (nth sorted-rows i)]
-                  [[:div prop (str (nth row 0))]
-                   [:div prop (str (nth row 1))]
-                   [:div (assoc prop :style (assoc (:style prop) :border-right "none"))
-                    (str (nth row 2))]])))]])))
+(def rows (r/atom (vec (for [i (range 100)]
+                         [(rand-int 100) i i]))))
+(defn grid-component [rows]
+  [:div
+   [header]
+   [:div {:style {:width "100%"
+                  :height "90%"
+                  :border "1px solid #ccc"
+                  :overflow "auto"
+                  :font-family "sans-serif"
+                  :font-size "14px"
+                  :position "relative"}}
+    (doall (for [i (range 100)
+                 :let [style {:flex "1" :padding "8px" :border-bottom "1px solid #eee" :border-right "1px solid #f9f9f9"}
+                       prop {:contentEditable "true"
+                             :style style}]]
+             (into [:div {:style {:display "flex"}
+                          :key i}
+                    [:div {:style {:width "20px" :height "20px" :padding "10px" :border-right "1px solid #ddd"}} (inc i)]]
+                   (let [row (nth @rows i)]
+                     [[:div prop (str (nth row 0))]
+                      [:div prop (str (nth row 1))]
+                      [:div (assoc prop :style (assoc (:style prop) :border-right "none"))
+                       (str (nth row 2))]]))))]])
 
 (defonce root (atom nil))
 
@@ -80,7 +78,7 @@
   (when-let [container (.getElementById js/document "app")]
     (when-not @root
       (reset! root (rdc/create-root container)))
-    (rdc/render @root [grid-component])))
+    (rdc/render @root [grid-component rows])))
 
 (defn init! []
   (mount-grid))
