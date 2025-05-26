64eeb1e54a674a3f08d8ff153a77fec93b991f78
Sonny To <son.c.to@gmail.com>
2025-05-26 12:46:53 +0700

    refactor handle-key-nav cell-component to take state as 1st param

--- src/cljs/lambdacart/grid.cljs
+++ src/cljs/lambdacart/grid.cljs
@@ -1,10 +1,6 @@
 (ns lambdacart.grid
-  (:require [reagent.core :as r]
-            [reagent.dom.client :as rdc]
+  (:require [reagent.dom.client :as rdc]
             [lambdacart.app :as app]))
-
-(def rows (r/atom (vec (for [i (range 100)]
-                         [(rand-int 100) i i]))))
 
 (defn header []
   (let [style {:style {:flex "1" :padding "10px" :border-right "1px solid #ddd"}}]
@@ -20,26 +16,27 @@
      [:div style "Description"]
      [:div style "Image"]]))
 
-(defn handle-key-nav [e current-idx current-row num-cols]
+(defn handle-key-nav [state e current-idx current-row num-cols]
   (let [key->direction {"ArrowLeft" [-1 0]
-                        "ArrowRight" [1 0]
-                        "ArrowUp" [0 -1]
-                        "ArrowDown" [0 1]}
-        [dx dy] (get key->direction (.-key e))]
+                       "ArrowRight" [1 0]
+                       "ArrowUp" [0 -1]
+                       "ArrowDown" [0 1]}
+        [dx dy] (get key->direction (.-key e))
+        rows (-> @state :grid :rows)]
     (when (and dx dy)
       (.preventDefault e)
       (let [next-idx (+ current-idx dx)
             next-row (+ current-row dy)
             next-el (when (and (>= next-idx 0) 
-                               (< next-idx num-cols)
-                               (>= next-row 0)
-                               (< next-row (count @rows)))
-                      (.querySelector js/document 
-                                      (str "[data-row='" next-row "'][data-col='" next-idx "']")))]
+                             (< next-idx num-cols)
+                             (>= next-row 0)
+                             (< next-row (count rows)))
+                     (.querySelector js/document 
+                                   (str "[data-row='" next-row "'][data-col='" next-idx "']")))]
         (when next-el
           (.focus next-el))))))
 
-(defn cell-component [value on-change row-idx col-idx]
+(defn cell-component [state value on-change row-idx col-idx]
   [:input {:type "text"
            :value value
            :data-row row-idx
@@ -49,7 +46,7 @@
                    :border "none"
                    :border-bottom "1px solid #eee" 
                    :border-right "1px solid #f9f9f9"}
-           :on-key-down #(handle-key-nav % col-idx row-idx 3)
+           :on-key-down #(handle-key-nav state % col-idx row-idx 3)
            :on-change #(on-change (.. % -target -value))}])
 
 (defn grid-component [state]
@@ -65,25 +62,28 @@
                     :position "relative"}}
       (doall 
        (for [[i row] (map-indexed (fn [i row]
-                                    [i row])
-                                  rows)]
+                                   [i row])
+                                 rows)]
          [:div {:style {:display "flex"}
                 :key i}
           [:div {:style {:width "20px" 
-                         :height "20px" 
-                         :padding "10px" 
-                         :border-right "1px solid #ddd"}} 
+                        :height "20px" 
+                        :padding "10px" 
+                        :border-right "1px solid #ddd"}} 
            (inc i)]
           [:<>
            [cell-component 
+            state
             (str (nth row 0)) 
             #(swap! rows assoc-in [i 0] %)
             i 0]
            [cell-component 
+            state
             (str (nth row 1)) 
             #(swap! rows assoc-in [i 1] %)
             i 1]
            [cell-component 
+            state
             (str (nth row 2)) 
             #(swap! rows assoc-in [i 2] %)
             i 2]]]))]]))
