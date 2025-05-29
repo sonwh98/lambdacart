7274d24fcb55bb585dec8d3619eaa0d8e535f431
Sonny To <son.c.to@gmail.com>
2025-05-29 17:20:26 +0700

    fmt

--- src/cljs/lambdacart/app.cljs
+++ src/cljs/lambdacart/app.cljs
@@ -1,11 +1,11 @@
 (ns lambdacart.app
   (:require [reagent.core :as r]))
 
-(defonce state 
+(defonce state
   (doto (r/atom {:grid {:rows []}
                  :context-menu {:visible? false :x 0 :y 0}})
     (add-watch :logger
-              (fn [_key _ref old-state new-state]
-                (prn "State changed:"
-                     {:old old-state
-                      :new new-state})))))
+               (fn [_key _ref old-state new-state]
+                 (prn "State changed:"
+                      {:old old-state
+                       :new new-state})))))
