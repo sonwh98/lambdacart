4250c8d9110cf8d626b8ffa9744fc8f87f6f515a
Sonny To <son.c.to@gmail.com>
2025-05-29 16:25:14 +0700

    log mutation

--- src/cljs/lambdacart/app.cljs
+++ src/cljs/lambdacart/app.cljs
@@ -1,4 +1,10 @@
 (ns lambdacart.app
   (:require [reagent.core :as r]))
 
-(defonce state (r/atom {:grid {:rows []}}))
+(defonce state 
+  (doto (r/atom {:grid {:rows []}})
+    (add-watch :logger
+              (fn [_key _ref old-state new-state]
+                (prn "State changed:"
+                     {:old old-state
+                      :new new-state})))))
