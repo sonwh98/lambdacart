2a7ae2722ad5a1abcd564a6a7009258ea4327cb3
Sonny To <son.c.to@gmail.com>
2025-07-04 18:18:31 +0700

    use CHP directly from lambda.cart/server

--- src/clj/lambdacart/server.clj
+++ src/clj/lambdacart/server.clj
@@ -1,7 +1,10 @@
 (ns lambdacart.server
   (:require [org.httpkit.server :as http]
             [stigmergy.server]
-            [clojure.core.async :as async]))
+            [stigmergy.chp]
+            [stigmergy.config :as c]
+            [clojure.core.async :as async]
+            [bidi.ring :as bidi] :reload))
 
 (defonce clients (atom []))
 
@@ -24,8 +27,26 @@
                      (fn [status]
                        (println "Channel closed:" status))))))
 
+(defn create-app []
+  (let [routes (c/config :bidi-routes)
+        handler (bidi/make-handler routes)
+        mime-types (merge {"chp" "text/html"
+                           nil "text/html"}
+                          (c/config :mime-types))
+        app (-> handler
+                (ring.middleware.file/wrap-file "public")
+                ring.middleware.params/wrap-params
+                (ring.middleware.content-type/wrap-content-type {:mime-types mime-types}))]
+    (fn [req]
+      (if (= (:uri req) "/ws")
+        (ws-handler req)
+        (app req)))))
+
 (defn -main []
-  (http/run-server ws-handler {:port 3002})
-  (println "WebSocket server running on ws://localhost:3002"))
+  (println "Loading configuration in" (System/getProperty "config"))
+  (c/reload)
+  (let [port (c/config :port)]
+    (http/run-server (create-app) {:port port})
+    (println (format "Server running on http://localhost:%s with WebSocket at ws://localhost:%s/ws", port port))))
 
 (comment)
