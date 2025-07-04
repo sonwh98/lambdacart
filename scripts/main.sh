e850219b8032e07c8a2deb6ecd0eeb4522dce938
Sonny To <son.c.to@gmail.com>
2025-07-04 18:25:16 +0700

    rm unnecessary scripts

--- scripts/main.sh
+++
@@ -1,6 +0,0 @@
-#clojure -J-Dclojure.server.repl="{:port 5555 :accept clojure.core.server/repl}" -J-Dconfig=./config.clj -X cybernut.core/main
-
-#clojure -J-Dconfig=./config.clj -X cybernut.core/main
-
-#clojure -M:nREPL -m nrepl.cmdline
-clojure -M:nREPL
