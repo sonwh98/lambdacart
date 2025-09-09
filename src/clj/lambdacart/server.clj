2c170f6d8b5de947e8d8d7c23372106bcaa2adc1
Sonny To <son.c.to@gmail.com>
2025-09-09 19:48:12 +0700

    implement server invoke

--- src/clj/lambdacart/server.clj
+++ src/clj/lambdacart/server.clj
@@ -19,15 +19,104 @@
 (defn broadcast [msg]
   (doseq [client @clients]
     (try
-      (prn "broadcast " msg)
-      (http/send! client msg)
-      (prn "broadcast2 " msg)
+      (if (string? msg)
+        (http/send! client msg)
+        (let [transit-data (serde/edn->transit msg)
+              bytes-data (.getBytes transit-data "UTF-8")]
+          (http/send! client bytes-data)))
       (catch Exception e
         (println "Failed to send message to client:" (.getMessage e))
         (swap! clients #(remove #{client} %))))))
 
-(defn invoke [data]
-  (println "invoke " data))
+;; Registry for available functions
+(defonce available-functions (atom {}))
+
+(defn register-function! [fn-name fn-impl]
+  (swap! available-functions assoc fn-name fn-impl))
+
+;; Register some example functions
+(register-function! 'ping 
+  (fn [& args]
+    (println "Ping called with args:" args)
+    {:type :pong :timestamp (java.util.Date.) :args args}))
+
+(register-function! 'echo 
+  (fn [message]
+    (println "Echo called with:" message)
+    {:type :echo-response :message message}))
+
+(register-function! 'update-cell 
+  (fn [row col value]
+    (println "Updating cell" row col "to" value)
+    ;; Broadcast the change to all connected clients
+    (broadcast {:type :cell-updated :row row :col col :value value})
+    {:type :update-success :row row :col col :value value}))
+
+(register-function! 'delete-rows
+  (fn [row-indices]
+    (println "Deleting rows:" row-indices)
+    ;; Broadcast the deletion to all connected clients
+    (broadcast {:type :rows-deleted :rows row-indices})
+    {:type :delete-success :rows row-indices}))
+
+(register-function! 'query-data 
+  (fn [filters & [pagination]]
+    (println "Querying data with filters:" filters "pagination:" pagination)
+    ;; Here you would query your database
+    {:type :query-results 
+     :data []  ; Replace with actual query results
+     :total-count 0
+     :filters filters
+     :pagination pagination}))
+
+(register-function! 'broadcast-message
+  (fn [message]
+    (println "Broadcasting message:" message)
+    (broadcast {:type :broadcast :message message :timestamp (java.util.Date.)})
+    {:type :broadcast-sent}))
+
+(register-function! 'get-stats
+  (fn []
+    {:type :stats-response 
+     :connected-clients (count @clients)
+     :available-functions (keys @available-functions)
+     :server-time (java.util.Date.)}))
+
+(defn send-response [channel response]
+  (when response
+    (let [response-data (serde/edn->transit response)
+          response-bytes (.getBytes response-data "UTF-8")]
+      (http/send! channel response-bytes))))
+
+(defn send-error [channel message]
+  (let [error-response {:type :error :message message}
+        error-data (serde/edn->transit error-response)
+        error-bytes (.getBytes error-data "UTF-8")]
+    (http/send! channel error-bytes)))
+
+(defn invoke [data channel]
+  (try
+    (if (and (sequential? data) (not (empty? data)))
+      (let [fn-name (first data)
+            args (rest data)
+            fn-impl (get @available-functions fn-name)]
+        (if fn-impl
+          (do
+            (println "Calling function:" fn-name "with args:" args)
+            (let [result (apply fn-impl args)]
+              (prn "sending result to client " result)
+              (send-response channel result)))
+          (do
+            (println "Unknown function:" fn-name)
+            (send-error channel (str "Unknown function: " fn-name ". Available: " 
+                                    (keys @available-functions))))))
+      (do
+        (println "Invalid message format. Expected (fn-name arg1 arg2 ...), got:" data)
+        (send-error channel "Invalid message format. Expected (fn-name arg1 arg2 ...)")))
+    (catch Exception e
+      (println "Error invoking function:" (.getMessage e))
+      (.printStackTrace e)
+      (send-error channel (str "Server error: " (.getMessage e))))))
 
 (defn ws-handler [req]
   (http/with-channel req channel
@@ -35,11 +124,18 @@
       (add-client! channel)
       (http/on-receive channel
                        (fn [transit-data]
-                         (let [_ (prn "transit-data" transit-data)
-                               edn-data (serde/transit->edn transit-data)
-                               _ (prn "foobar")]
-                           (invoke edn-data)
-                           (http/send! channel (str "Echo: " edn-data)))))
+                         (try
+                           (let [edn-data (if (bytes? transit-data)
+                                            ;; Handle binary data
+                                            (let [json-str (String. transit-data "UTF-8")]
+                                              (serde/transit->edn json-str))
+                                            ;; Handle text data
+                                            (serde/transit->edn transit-data))]
+                             (println "Received function call:" edn-data)
+                             (invoke edn-data channel))
+                           (catch Exception e
+                             (println "Error processing WebSocket data:" (.getMessage e))
+                             (send-error channel "Invalid data format")))))
       (http/on-close channel
                      (fn [status]
                        (println "Channel closed:" status)
@@ -68,7 +164,8 @@
   (let [port (c/config :port)
         s (http/run-server (create-app) {:port port})]
     (reset! server s)
-    (println (format "Server running on http://localhost:%s with WebSocket at ws://localhost:%s/wsstream" port port))))
+    (println (format "Server running on http://localhost:%s with WebSocket at ws://localhost:%s/wsstream" port port))
+    (println "Available RPC functions:" (keys @available-functions))))
 
 (defn stop-server []
   (when @server
@@ -85,4 +182,17 @@
   (start-server)
   (stop-server)
   (broadcast "wassup")
-  (count @clients))
+  (broadcast {:type :notification :message "Hello all clients!"})
+  (count @clients)
+  
+  ;; Check available functions
+  @available-functions
+  
+  ;; Register new functions at runtime
+  (register-function! 'add
+    (fn [a b]
+      {:type :math-result :operation :add :result (+ a b)}))
+  
+  (register-function! 'get-time
+    (fn []
+      {:type :time-response :time (java.util.Date.)})))
