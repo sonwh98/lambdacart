4ad34237d0a1662f81e281beb6738c28d22b7e73
Sonny To <son.c.to@gmail.com>
2025-09-11 13:23:58 +0700

    start webserver when user ns loaded

--- src/dev/user.clj
+++ src/dev/user.clj
@@ -1,14 +1,19 @@
 (ns user
-  (:require [datomic.api :as d]
-            [datomic.db]
-            [datomic.function]
-            [datomic.codec]
-            [clojure.edn :as edn]))
+  "Development namespace that auto-starts the server"
+  (:require [lambdacart.server :as server]
+            [shadow.cljs.devtools.api :as shadow]))
 
 
-(comment
-  
+(defn start-dev []
+  (println "Starting development environment...")
+  (server/start-server)
+  (println "Server started. You can now run: (shadow/watch :grid)"))
 
-  ) 
+(defn stop-dev []
+  (println "Stopping development environment...")
+  (server/stop-server))
+
+;; Auto-start when this namespace loads
+(start-dev)
 
 
