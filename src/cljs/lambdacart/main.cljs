e1f6f6e00d913788a1035c7bf66aa6e88818f85e
Sonny To <son.c.to@gmail.com>
2025-09-23 16:33:23 +0800

    extract header items-grid

--- src/cljs/lambdacart/main.cljs
+++ src/cljs/lambdacart/main.cljs
@@ -11,7 +11,7 @@
 (defn get-all-items [store]
   "Get all items from all catalogs and tagories in store data"
   (->> store
-       :catalogs  
+       :catalogs
        (mapcat :tagories)
        (mapcat :items)
        (vec)))
@@ -38,70 +38,75 @@
                              :display-items (:items tagory)))}
         (:tagory/name tagory)])]))
 
+(defn items-grid [display-items]
+  [:div.card-grid
+   (for [item @display-items]
+     [:div.card {:key (:item/id item)
+                 :data-item-id (str (:id item))
+                 :style {:display "flex"
+                         :flex-direction "column"
+                         :height "450px"}} ; Fixed height for all cards
+      [:img {:src (-> item :item/images first :image/url)
+             :alt (:item/name item)
+             :style {:width "100%"
+                     :height "200px"
+                     :object-fit "cover"
+                     :flex-shrink 0}}] ; Image won't shrink
+      [:div.card-content
+       {:style {:display "flex"
+                :flex-direction "column"
+                :flex-grow 1
+                :padding "16px"}}
+       [:h3 {:style {:margin "0 0 8px 0"}} (:item/name item)]
+       [:p {:style {:margin "0 0 auto 0" ; Push everything below down
+                    :overflow "hidden"
+                    :display "-webkit-box"
+                    :-webkit-line-clamp 4 ; Limit to 4 lines
+                    :-webkit-box-orient "vertical"
+                    :text-overflow "ellipsis"
+                    :flex-grow 1}}
+        (:item/description item)]
+
+       ;; Price and button always at bottom
+       [:div {:style {:margin-top "auto"}} ; This pushes to bottom
+        [:div.price
+         {:style {:font-weight :bold :color "#e91e63" :font-size "1.2em"
+                  :margin-bottom "10px"}}
+         "$" (gstring/format "%.2f" (/ (:item/price item) 100.0))]
+        [:button.add-to-cart-btn
+         {:style {:background "#e91e63"
+                  :color "white"
+                  :border "none"
+                  :border-radius "5px"
+                  :padding "8px 16px"
+                  :cursor "pointer"
+                  :width "100%"}
+          :on-click #(add-to-cart item)}
+         "Add to Cart"]]]])])
+
+(defn header [state]
+  [:div.header-container
+   [:div.search-container
+    [:input.search-box {:type "text" :placeholder "Search cosmetics..."}]]
+   [:button.menu-toggle {:onclick "lambdacart.cosmetics.toggleMenu()"}
+    [:span.hamburger]]
+   [:nav.navigation
+    (let [active-tagory (:active-tagory @state)]
+      [:div.tab-bar
+       [:button.tab {:class (if (or (nil? active-tagory)
+                                    (= active-tagory {:tagory/name :all}))
+                              "active")
+                     :on-click #(let [all-items (get-all-items (:store @state))]
+                                  (swap! state assoc
+                                         :active-tagory nil
+                                         :display-items all-items))}
+        "All Products"]
+       [tabs state]])]])
+
 (defn main-ui [state]
   [:div
-   [:div.header-container
-    [:div.search-container
-     [:input.search-box {:type "text" :placeholder "Search cosmetics..."}]]
-    [:button.menu-toggle {:onclick "lambdacart.cosmetics.toggleMenu()"}
-     [:span.hamburger]]
-    [:nav.navigation
-     (let [active-tagory (:active-tagory @state)]
-       [:div.tab-bar
-        [:button.tab {:class (if (or (nil? active-tagory)
-                                     (= active-tagory {:tagory/name :all}))
-                               "active")
-                      :on-click #(let [all-items (get-all-items (:store @state))]
-                                   (swap! app/state assoc
-                                          :active-tagory nil
-                                          :display-items all-items))}
-         "All Products"]
-        [tabs state]])]]
-
-   [:div.card-grid
-    (for [item (:display-items @state)]
-      [:div.card {:key (:item/id item)
-                  :data-item-id (str (:id item))
-                  :style {:display "flex"
-                          :flex-direction "column"
-                          :height "450px"}}  ; Fixed height for all cards
-       [:img {:src (-> item :item/images first :image/url)
-              :alt (:item/name item)
-              :style {:width "100%" 
-                      :height "200px" 
-                      :object-fit "cover"
-                      :flex-shrink 0}}]  ; Image won't shrink
-       [:div.card-content
-        {:style {:display "flex"
-                 :flex-direction "column"
-                 :flex-grow 1
-                 :padding "16px"}}
-        [:h3 {:style {:margin "0 0 8px 0"}} (:item/name item)]
-        [:p {:style {:margin "0 0 auto 0"  ; Push everything below down
-                     :overflow "hidden"
-                     :display "-webkit-box"
-                     :-webkit-line-clamp 4  ; Limit to 4 lines
-                     :-webkit-box-orient "vertical"
-                     :text-overflow "ellipsis"
-                     :flex-grow 1}} 
-         (:item/description item)]
-        
-        ;; Price and button always at bottom
-        [:div {:style {:margin-top "auto"}}  ; This pushes to bottom
-         [:div.price
-          {:style {:font-weight :bold :color "#e91e63" :font-size "1.2em"
-                   :margin-bottom "10px"}}
-          "$" (gstring/format "%.2f" (/ (:item/price item) 100.0))]
-         [:button.add-to-cart-btn
-          {:style {:background "#e91e63"
-                   :color "white"
-                   :border "none"
-                   :border-radius "5px"
-                   :padding "8px 16px"
-                   :cursor "pointer"
-                   :width "100%"}
-           :on-click #(add-to-cart item)}
-          "Add to Cart"]]]])]])
+   [header state]
+   [items-grid (r/cursor state [:display-items])]])
 
 (defonce root (atom nil))
 
