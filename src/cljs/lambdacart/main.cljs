6f50c55abcd729ed61e949da07f479de924332da
Sonny To <son.c.to@gmail.com>
2025-09-30 22:56:20 +0800

    add cart-icon as a tab

--- src/cljs/lambdacart/main.cljs
+++ src/cljs/lambdacart/main.cljs
@@ -54,6 +54,13 @@
   (-> @app/state :cart type)
   (-> @app/state :cart count))
 
+(defn cart-icon []
+  [:svg {:xmlns "http://www.w3.org/2000/svg"
+         :width "24" :height "24" :viewBox "0 0 24 24" :fill "none" :stroke "currentColor" :stroke-width "2" :stroke-linecap "round" :stroke-linejoin "round" :style {:vertical-align "middle" :margin-right "4px"}}
+   [:circle {:cx "9" :cy "21" :r "1"}]
+   [:circle {:cx "20" :cy "21" :r "1"}]
+   [:path {:d "M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"}]])
+
 (defn tabs [state]
   (let [active-tagory (:active-tagory @state)
         tagories (get-in @state [:store :catalogs 0 :tagories])]
@@ -81,7 +88,8 @@
                        (swap! app/state assoc
                               :active-tagory active-tagory
                               :display-items (:items tagory)))}
-         (:tagory/name tagory)])]]))
+         (:tagory/name tagory)])
+      [cart-icon]]]))
 
 (defn items-grid [display-items]
   [:div.card-grid
