547f668a3b6cd780b656cb3c4574ab721c96e6df
Sonny To <son.c.to@gmail.com>
2025-10-04 15:22:01 +0800

    / -> main.chp

--- config.clj
+++ config.clj
@@ -2,5 +2,6 @@
  :chp-dir "public/chp"
  :public-dir "public"
  :image-upload-dir "public/images"
- :bidi-routes ["/" [[#".*\.chp"  stigmergy.chp/hiccup-page-handler]
+ :bidi-routes ["/" [["" (fn [req] (stigmergy.chp/hiccup-page-handler (assoc req :uri "/main.chp")))]
+                    [#".*\.chp"  stigmergy.chp/hiccup-page-handler]
                     ["upload" lambdacart.server/upload-handler]]]}
